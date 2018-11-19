f9da66a05c58a794b0fa4c80a6997095b1eb91ce
Ivan Maistrenka <Ivan_Maistrenka@epam.com>
2018-11-19 12:51:53 +0300

    Webpack and babel config

---
+++ client/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const htmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+    entry: require.resolve("./src/index.js"),
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "bundle.js"
+    },
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+				exclude: /node_modules/,
+                loader: "babel-loader",
+				options: {
+				  presets: ['latest'],
+				  plugins: ['transform-runtime']
+			    }
+            },
+			{
+			    test: /\.css$/,
+			    use: ['style-loader', 'css-loader'],
+		    }
+        ]
+    },
+	plugins: [
+       new htmlWebpackPlugin({
+         template: './src/index.html',
+         filename: '../index.html',
+         hash: true
+       })
+  ]
+};
